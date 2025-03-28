595ecc9edb0a8e6ddf4f5305b7c482a4a02ed1e2
Atri <chaudhuriatri@gmail.com>
2025-03-29 01:08:23 +0530

    bugs fixed

--- game.py
+++ game.py
@@ -79,7 +79,8 @@ class PokerGame:
         bb_player = self.players[bb_position]
 
         while bb_player.status != PlayerStatus.ACTIVE:
-            bb_position = (self.button_position + 1) % len(self.players)
+            #print("test")
+            bb_position = (bb_position + 1) % len(self.players)
             bb_player = self.players[bb_position]
 
         if bb_player.stack > 0:
