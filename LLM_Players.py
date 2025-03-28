78dd381c5b451cba1e827430ea2d6740b6d7bce0
Tanish-0001 <jaintanish2005@gmail.com>
2025-03-28 15:48:46 +0530

    added

--- LLM_Players.py
+++ LLM_Players.py
@@ -1,13 +1,17 @@
 from langchain.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from player import Player, PlayerAction
+from json import loads
 
 
 class LLMPlayer(Player):
 
     def __init__(self, name, stack, api_key):
         super().__init__(name, stack)
-        self.llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0, google_api_key=api_key)
+        self.llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash",
+                                          temperature=0,
+                                          response_format={"type": "json_object"},
+                                          google_api_key=api_key)
 
         template = """You are a professional poker player. You must win both, a lot of games and a lot of money. You are allowed to take safe risks.
                 \nYou will be given the action history and the current poker game state as a sequence of numbers
@@ -22,8 +26,12 @@ class LLMPlayer(Player):
                 \n\nHere is the game state, as a list of numbers:
                 {game_state}
                 \nYou are {name} and you currently have {stack} dollars.
-                \nYou must provide only the action and the amount, and nothing else. Saying anything else will disqualify you and forcefully fold your hand.
-                \n\nYour output must be in the format:\nACTION, AMOUNT
+                \nYou must return the output in the following JSON format:\n
+                [
+                "ACTION": <FOLD/CALL/RAISE>,
+                "AMOUNT": <integer>,
+                "EXPLANATION": <a brief reasoning>
+                ]
                 """
 
         self.prompt = ChatPromptTemplate.from_template(template)
@@ -34,16 +42,21 @@ class LLMPlayer(Player):
         call_amount = game_state[8] - self.bet_amount
         output = self.chain.invoke(
             {'action_history': action_history, 'game_state': game_state, 'name': self.name, 'stack': self.stack})
+        parsed_output = output.content.strip("```")
+        parsed_output = parsed_output.lstrip("json")
 
         try:
-            action, amount = output.content.split(',')
+            json = loads(parsed_output)
+            action, amount = json["ACTION"], json["AMOUNT"]
             amount = int(amount)
             if action.upper() == 'CALL':
                 return PlayerAction.CALL, call_amount
             elif action.upper() == 'RAISE' and amount > max(game_state[-2], game_state[8]):
                 return PlayerAction.RAISE, int(amount)
             return PlayerAction.FOLD, 0
-        except:
+
+        except Exception as e:
+            print(f"Encountered error: {e}")
             return PlayerAction.FOLD, 0
 
     @staticmethod
